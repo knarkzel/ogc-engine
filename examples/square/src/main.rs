65009e53acf8a4d8e73338dea81a1df4f935341b
Knarkzel <knarkzel@gmail.com>
2021-07-10 06:16:39 +0200

    added easier example

--- examples/square/src/main.rs
+++ examples/square/src/main.rs
@@ -3,10 +3,7 @@
 
 use ogc_engine::prelude::*;
 
-use embedded_graphics::{
-    pixelcolor::Rgb888,
-    primitives::{PrimitiveStyleBuilder, Rectangle},
-};
+use embedded_graphics::{pixelcolor::Rgb888, primitives::Rectangle};
 
 struct Game {
     x: i32,
@@ -15,14 +12,9 @@ struct Game {
 
 impl State for Game {
     fn update(&mut self, _video: &mut Video, display: &mut Display) {
-        Rectangle::new(Point::new(self.x, self.y), Size::new(50, 50))
-            .into_styled(
-                PrimitiveStyleBuilder::new()
-                    .fill_color(Rgb888::WHITE)
-                    .build(),
-            )
-            .draw(display)
-            .unwrap();
+        let rectangle = Rectangle::new(Point::new(self.x, self.y), Size::new(50, 50));
+        display.fill_solid(&rectangle, Rgb888::WHITE).unwrap();
+        display.clear(Rgb888::new(150, 150, 150)).unwrap();
         self.x = (self.x + 1) % 640;
         self.y = (self.y + 2) % 528;
     }
